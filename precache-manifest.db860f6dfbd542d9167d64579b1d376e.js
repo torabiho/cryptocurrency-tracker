508156424f5c58cb1a07421b8d7b32de6e0f1572
Hossein <hossein.tb@gmail.com>
2020-09-07 17:34:46 -0400

    Updates

---
+++ precache-manifest.db860f6dfbd542d9167d64579b1d376e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc5d7a15fef37b5d2cda6e9965909dc",
+    "url": "/torabiho/cryptocurrency-tracker/index.html"
+  },
+  {
+    "revision": "3af9559f1157f1fb4077",
+    "url": "/torabiho/cryptocurrency-tracker/static/css/main.f3df0e7f.chunk.css"
+  },
+  {
+    "revision": "4e35a1864aee2b94f233",
+    "url": "/torabiho/cryptocurrency-tracker/static/js/2.1ac851fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/torabiho/cryptocurrency-tracker/static/js/2.1ac851fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3af9559f1157f1fb4077",
+    "url": "/torabiho/cryptocurrency-tracker/static/js/main.30df2b27.chunk.js"
+  },
+  {
+    "revision": "21270d6954b572c2754c",
+    "url": "/torabiho/cryptocurrency-tracker/static/js/runtime-main.3db5829d.js"
+  }
+]);
